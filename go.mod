dee2f26868c7ac518a0b8f027a3dfe01f473358e
Kaustubh G <gayadhankarkaustubh@gmail.com>
2022-01-18 17:08:05 +0530

    commit 2

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/KG-9991/Go_time_api
+
+go 1.17
+
+require github.com/gorilla/mux v1.8.0
--- time.go
+++ time.go
@@ -1,5 +1,15 @@
 package main
 
-func main(){
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/time", getTime).Methods(http.MethodGet)
 	
+
 }
